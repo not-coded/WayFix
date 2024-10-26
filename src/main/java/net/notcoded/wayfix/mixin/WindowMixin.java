8e6c475664db11de2331bd64921e5451237ab0c9
NotCoded <66999075+not-coded@users.noreply.github.com>
2024-10-26 14:36:09 +0200

    update stonecutter + switch to 1.20.6

--- src/main/java/net/notcoded/wayfix/mixin/WindowMixin.java
+++ src/main/java/net/notcoded/wayfix/mixin/WindowMixin.java
@@ -20,18 +20,18 @@ import static net.notcoded.wayfix.WayFix.isWayland;
 
 //? if >=1.20 {
 
-/*import net.minecraft.client.util.Icons;
+import net.minecraft.client.util.Icons;
 import net.minecraft.resource.ResourcePack;
 import java.io.IOException;
-*///?} elif 1.19.3 {
-import java.util.ArrayList;
+//?} elif 1.19.3 {
+/*import java.util.ArrayList;
 import java.util.Arrays;
 import net.minecraft.resource.InputSupplier;
-//?}
+*///?}
 
 //? if <1.20 {
-import java.io.InputStream;
-//?}
+/*import java.io.InputStream;
+*///?}
 
 /*
 - Credits to moehreag for most of the code
@@ -73,21 +73,21 @@ public class WindowMixin {
     @Inject(method = "setIcon", at = @At("HEAD"), cancellable = true)
     //? if >=1.20 {
     
-    /*private void injectIcon(ResourcePack resourcePack, Icons icons, CallbackInfo ci) {
-    *///?} elif 1.19.3 {
-    private void injectIcon(InputSupplier<InputStream> smallIconSupplier, InputSupplier<InputStream> bigIconSupplier, CallbackInfo ci) {
-    //?} elif <1.19.3 {
+    private void injectIcon(ResourcePack resourcePack, Icons icons, CallbackInfo ci) {
+    //?} elif 1.19.3 {
+    /*private void injectIcon(InputSupplier<InputStream> smallIconSupplier, InputSupplier<InputStream> bigIconSupplier, CallbackInfo ci) {
+    *///?} elif <1.19.3 {
     
     /*private void injectIcon(InputStream icon16, InputStream icon32, CallbackInfo ci) {
     *///?}
         if (isWayland()) {
             //? if >=1.20 {
-            /*try {
+            try {
                 DesktopFileInjector.setIcon(icons.getIcons(resourcePack));
             } catch (IOException ignored) { }
-            *///?} elif 1.19.3 {
-            DesktopFileInjector.setIcon(new ArrayList<>(Arrays.asList(smallIconSupplier, bigIconSupplier)));
-            //?} elif <1.19.3 {
+            //?} elif 1.19.3 {
+            /*DesktopFileInjector.setIcon(new ArrayList<>(Arrays.asList(smallIconSupplier, bigIconSupplier)));
+            *///?} elif <1.19.3 {
             
             /*DesktopFileInjector.setIcon(icon16, icon32);
             *///?}
