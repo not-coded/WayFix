a3c85263c827f10010f3707a02695dd95658a4d3
NotCoded <66999075+not-coded@users.noreply.github.com>
2024-11-01 23:16:34 +0100

    mixin moment

--- neoforge/src/main/java/net/notcoded/wayfix/forge/WayFixNeoForge.java
+++ neoforge/src/main/java/net/notcoded/wayfix/forge/WayFixNeoForge.java
@@ -8,7 +8,7 @@ import net.neoforged.fml.ModLoadingContext;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 import net.neoforged.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
+import net.neoforged.neoforge.client.ConfigScreenHandler;
 import net.notcoded.wayfix.common.config.ModConfig;
 
 @Mod(value = "wayfix")
@@ -22,8 +22,8 @@ public class WayFixNeoForge {
     private static void onClientSetup(final FMLClientSetupEvent event) {
         // TODO: add config support for >1.20.6 (IConfigScreenFactory)
         event.enqueueWork(() -> {
-            ModLoadingContext.get().registerExtensionPoint(IConfigScreenFactory.class, () ->
-                    (
+            ModLoadingContext.get().registerExtensionPoint(ConfigScreenHandler.ConfigScreenFactory.class, () ->
+                    new ConfigScreenHandler.ConfigScreenFactory(
                             (client, parent) -> AutoConfig.getConfigScreen(ModConfig.class, parent).get()
                     )
             );
