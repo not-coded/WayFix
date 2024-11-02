8fc50d6faf0c5c173f2617aca3d08cf6b606cecf
NotCoded <66999075+not-coded@users.noreply.github.com>
2024-11-02 15:35:59 +0100

    fix config button not working

--- forge/src/main/java/net/notcoded/wayfix/forge/WayFixForge.java
+++ forge/src/main/java/net/notcoded/wayfix/forge/WayFixForge.java
@@ -8,33 +8,28 @@ import net.minecraftforge.client.ConfigScreenHandler;
 //?} elif <1.19 {
 /*import net.minecraftforge.fml.ExtensionPoint;
  *///?}
-import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.notcoded.wayfix.common.config.ModConfig;
 
 @Mod(value = "wayfix")
 @OnlyIn(Dist.CLIENT)
 public class WayFixForge {
     public WayFixForge() {
-        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
-        modEventBus.addListener(WayFixForge::onClientSetup);
+        DistExecutor.safeRunWhenOn(Dist.CLIENT, () -> WayFixForge::setupConfigScreen);
     }
 
-    private static void onClientSetup(final FMLClientSetupEvent event) {
-        event.enqueueWork(() -> {
-            //? if >=1.19 {
-            ModLoadingContext.get().registerExtensionPoint(ConfigScreenHandler.ConfigScreenFactory.class, () ->
-                    new ConfigScreenHandler.ConfigScreenFactory(
-                            (client, parent) -> AutoConfig.getConfigScreen(ModConfig.class, parent).get()
-                    )
-            );
-            //?} elif <1.19 {
-            /*ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.CONFIGGUIFACTORY, () ->
-                    (client, parent) -> AutoConfig.getConfigScreen(ModConfig.class, parent).get()
-            );*///?}
-        });
+    private static void setupConfigScreen() {
+        //? if >=1.19 {
+        ModLoadingContext.get().registerExtensionPoint(ConfigScreenHandler.ConfigScreenFactory.class, () -> 
+                new ConfigScreenHandler.ConfigScreenFactory(
+                        (client, parent) -> AutoConfig.getConfigScreen(ModConfig.class, parent).get()
+                )
+        );
+        //?} elif <1.19 {
+        /*ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.CONFIGGUIFACTORY, () ->
+                (client, parent) -> AutoConfig.getConfigScreen(ModConfig.class, parent).get()
+        );*///?}
     }
 }
