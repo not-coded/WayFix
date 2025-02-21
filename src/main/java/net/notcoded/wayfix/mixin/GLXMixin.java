64ea21aea3d74195c90c2dc3dcbbeca89fc8c650
NotCoded <66999075+not-coded@users.noreply.github.com>
2025-02-21 20:47:42 +0100

    awesome stonecutter

--- src/main/java/net/notcoded/wayfix/mixin/GLXMixin.java
+++ src/main/java/net/notcoded/wayfix/mixin/GLXMixin.java
@@ -2,7 +2,7 @@ package net.notcoded.wayfix.mixin;
 
 import com.mojang.blaze3d.platform.GLX;
 import org.spongepowered.asm.mixin.Mixin;
-//? if !fabric && (1.16.5 || >=1.20.1) {
+//? if (forge || neoforge) && (1.16.5 || >=1.20.1) {
 import org.lwjgl.glfw.GLFW;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -20,7 +20,7 @@ import static net.notcoded.wayfix.WayFix.supportsWayland;
 
 @Mixin(GLX.class)
 public abstract class GLXMixin {
-    //? if !fabric && (1.16.5 || >=1.20.1) {
+    //? if (forge || neoforge) && (1.16.5 || >=1.20.1) {
 
     @Inject(method = "_initGlfw", at = @At("HEAD"), remap = false)
     private static void preGLFWInit(CallbackInfoReturnable<LongSupplier> cir) {
