8ca5e2fa392d7b26b5ca1b310bb669a4368e1488
NotCoded <66999075+not-coded@users.noreply.github.com>
2025-12-18 20:58:16 +0100

    1.21.9 (neo)forge support

--- src/main/java/net/notcoded/wayfix/mixin/KeyboardMixin.java
+++ src/main/java/net/notcoded/wayfix/mixin/KeyboardMixin.java
@@ -1,18 +1,14 @@
 package net.notcoded.wayfix.mixin;
 
+import io.netty.util.internal.PlatformDependent;
 import net.minecraft.client.Keyboard;
-
-//? if >=1.21.9 {
-/*import net.minecraft.client.input.CharInput;
-import net.minecraft.client.input.SystemKeycodes;
-*///?} else if <1.21.9 {
-import net.minecraft.client.gui.screen.Screen;
-import net.minecraft.client.util.InputUtil;
-//?}
+import net.minecraft.client.MinecraftClient;
 import org.lwjgl.glfw.GLFW;
 import net.notcoded.wayfix.WayFix;
 
+import org.spongepowered.asm.mixin.Dynamic;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -26,25 +22,20 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(Keyboard.class)
 public class KeyboardMixin {
 
-    @Inject(method = "onChar", at = @At("HEAD"), cancellable = true)
-    //? if <1.21.9 {
-    private void charTyped(long window, int codePoint, int modifiers, CallbackInfo ci) {
-    //?}
-    //? if >=1.21.9 {
-    /*private void charTyped(long window, CharInput input, CallbackInfo ci) {
-    *///?}
+    @Unique
+    private static final int LEFT_CTRL = PlatformDependent.isOsx() ? GLFW.GLFW_KEY_LEFT_SUPER : GLFW.GLFW_KEY_LEFT_CONTROL;
+    @Unique
+    private static final int RIGHT_CTRL = PlatformDependent.isOsx() ? GLFW.GLFW_KEY_RIGHT_SUPER : GLFW.GLFW_KEY_RIGHT_CONTROL;
+
+    @Dynamic
+    @Inject(method = {"onChar", "method_1457"}, at = @At("HEAD"), cancellable = true, require = 1)
+    private void charTyped(CallbackInfo ci) {
         if(!WayFix.config.keyModifiersFix || !WayFix.isWayland()) return;
 
-        //? if >=1.21.9 {
-        /*if(GLFW.glfwGetKey(window, SystemKeycodes.LEFT_CTRL) == GLFW.GLFW_PRESS ||
-                GLFW.glfwGetKey(window, SystemKeycodes.RIGHT_CTRL) == GLFW.GLFW_PRESS ||
+        long window = MinecraftClient.getInstance().getWindow().getHandle();
+        if(GLFW.glfwGetKey(window, LEFT_CTRL) == GLFW.GLFW_PRESS ||
+                GLFW.glfwGetKey(window, RIGHT_CTRL) == GLFW.GLFW_PRESS ||
                 GLFW.glfwGetKey(window, GLFW.GLFW_KEY_LEFT_ALT) == GLFW.GLFW_PRESS
-        *///?}
-        //? if <1.21.9 {
-        if(
-                Screen.hasControlDown() ||
-                        InputUtil.isKeyPressed(window, GLFW.GLFW_KEY_LEFT_ALT)
-        //?}
         ) {
             ci.cancel();
         }
--- stonecutter.gradle.kts
+++ stonecutter.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("dev.kikugie.stonecutter")
-    id("dev.architectury.loom") version "1.13.457" apply false
-    id("architectury-plugin") version "3.4.162" apply false
+    id("dev.architectury.loom") version "1.13.465" apply false
+    id("architectury-plugin") version "3.4-SNAPSHOT" apply false
     id("com.github.johnrengelman.shadow") version "8.1.1" apply false
     id("me.modmuss50.mod-publish-plugin") version "1.1.0" apply false
 }
